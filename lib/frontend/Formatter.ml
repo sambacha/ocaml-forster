78a02f21c37f951d7e3d759b93d25d0d07b19d49
Kento Okura <kentookura@gmail.com>
2024-08-08 09:40:48 +0000

    add code formatter

---
+++ lib/frontend/Formatter.ml
@@ -0,0 +1,140 @@
+open Pretty_expressive
+open Forester_core
+
+let w = 80
+let cf = Printer.default_cost_factory ~page_width:w ()
+
+module P = Printer.Make ((val cf))
+open P
+
+let backslash c = text "\\" ^^ c
+let braces c = text "{" ^^ c ^^ text "}"
+let _textcmd label arg = backslash @@ text label ^^ braces (text arg)
+let cmd t n = text "\\" ^^ text t ^^ text "{" ^^ n ^^ text "}"
+let mapt f (a, b) = (f a, f b)
+
+let delim d =
+  let t =
+    match d with
+    | Squares -> ("[", "]")
+    | Braces -> ("{", "}")
+    | Parens -> ("(", ")")
+  in
+  mapt text t
+
+let mode m =
+  let t = match m with Display -> ("##{", "}") | Inline -> ("#{", "}") in
+  mapt text t
+
+let wrap t n = fst t ^^ n ^^ snd t
+let braces = wrap (delim Braces)
+let parens = wrap (delim Parens)
+let squares = wrap (delim Squares)
+let twice f x = f (f x)
+
+exception Todo
+
+let format_symbol sym = text @@ Format.asprintf "%a" Symbol.pp sym
+
+let pp_trie_path ppf segments =
+  let pp_sep ppf () = Format.fprintf ppf "/" in
+  Format.fprintf ppf "%a"
+    Format.(pp_print_list ~pp_sep pp_print_string)
+    segments
+
+let format_trie_path p = text @@ Format.asprintf "%a" pp_trie_path p
+
+let rec f (n : Code.node) =
+  match n with
+  | Code.Text s -> text s
+  | Code.Verbatim s -> text s
+  | Code.Group (d, nodes) ->
+      let d =
+        match d with Braces -> braces | Squares -> squares | Parens -> parens
+      in
+      d @@ format_code nodes
+  | Code.Math (m, nodes) -> wrap (mode m) @@ format_code nodes
+  | Code.Ident (path, methods) ->
+      let methods =
+        List.map (fun str -> text "#" ^^ text str) methods |> fold_doc ( ^^ )
+      in
+      text "\\" ^^ format_trie_path path ^^ methods
+  | Code.Xml_tag (title, attrs, nodes) ->
+      let title =
+        match fst title with
+        | None -> text @@ snd title
+        | Some t -> text t ^^ text ":" ^^ text @@ snd title
+      in
+      text "\\<" ^^ title ^^ text ">"
+      ^^ fold_doc ( <+> ) (List.map format_attr attrs)
+      ^^ braces @@ format_code nodes
+  | Code.Subtree (addr, nodes) ->
+      let addr =
+        match addr with None -> empty | Some addr -> squares @@ text addr
+      in
+      text "\\subtree" ^^ addr ^^ braces @@ format_code nodes
+  | Code.Let (path, bindings, nodes) ->
+      text "\\let" ^^ format_trie_path path
+      ^^ fold_doc ( <+> ) (List.map format_binding bindings)
+      ^^ braces @@ format_code nodes
+  | Code.Open path -> text "\\open\\" ^^ format_trie_path path
+  | Code.Scope nodes -> text "\\scope" ^^ braces @@ format_code nodes
+  | Code.Put (path, nodes) ->
+      text "\\put\\" ^^ format_trie_path path ^^ braces @@ format_code nodes
+  | Code.Default (path, nodes) ->
+      text "\\put?\\" ^^ format_trie_path path ^^ braces @@ format_code nodes
+  | Code.Get path -> format_trie_path path
+  | Code.Fun (bindinglist, nodes) ->
+      text "\\fun"
+      ^^ fold_doc ( <+> ) (List.map format_binding bindinglist)
+      ^^ braces @@ format_code nodes
+  | Code.Object { self; methods } ->
+      let self =
+        match self with Some path -> format_trie_path path | None -> empty
+      in
+      text "\\object" ^^ (squares @@ self)
+      ^^ fold_doc ( <+> ) (List.map format_method methods)
+  | Code.Patch patch -> (
+      match patch with
+      | { obj; self; methods } ->
+          let self =
+            match self with Some self -> format_trie_path self | None -> empty
+          in
+          text "\\patch"
+          ^^ (braces @@ format_code obj)
+          ^^ squares self ^^ braces
+          @@ fold_doc ( <+> ) (List.map format_method methods))
+  | Code.Call (obj, method_name) ->
+      text "\\call" ^^ format_code obj ^^ text method_name
+  | Code.Import (vis, str) ->
+      let ident =
+        match vis with Private -> text "\\import" | Public -> text "\\export"
+      in
+      ident ^^ braces @@ text str
+  | Code.Def (path, bindinglist, nodes) ->
+      text "\\def\\" ^^ format_trie_path path
+      ^^ fold_doc ( <+> ) (List.map format_binding bindinglist)
+      ^^ braces @@ format_code nodes
+  | Code.Decl_xmlns (prefix, xmlns) ->
+      text "\\xmlns:" ^^ text prefix ^^ braces @@ text xmlns
+  | Code.Alloc path -> text "\\alloc\\" ^^ format_trie_path path
+  | Code.Namespace (path, code) ->
+      text "\\namespace" ^^ format_trie_path path ^^ format_code code
+
+and format_method ((name, nodes) : string * Code.t) =
+  (squares @@ text name) ^^ braces @@ format_code nodes
+
+and format_binding ((strategy, path) : Forester_core__Trie.path Base.binding) =
+  let strat = match strategy with Lazy -> text "~" | Strict -> empty in
+  squares @@ strat ^^ format_trie_path path
+
+and format_attr (((prefix, uname), v) : (string option * string) * Code.t) =
+  let prefix =
+    match prefix with None -> empty | Some str -> squares @@ text str
+  in
+  prefix ^^ (squares @@ text uname) ^^ braces @@ format_code v
+
+and format_code (ns : Code.t) =
+  List.map (fun n -> f Range.(n.value)) ns |> fold_doc (fun x y -> x <+> y)
+
+let print_tree (t : Code.t) = format_code t |> pretty_print print_string
--- lib/ui/Repl.ml
+++ lib/ui/Repl.ml
@@ -1,8 +1,7 @@
 open Forester_core
+open Forester_render
 open Forester_frontend
-open Nottui
 open Lwd_infix
-module Widget = Nottui_widgets
 
 module Cache_handle = Algaeff.State.Make (struct
   type t = Irmin_defs.Store.t
@@ -11,16 +10,19 @@ end)
 let make_dir ~env dir = Eio.Path.(Eio.Stdenv.fs env / dir)
 let make_dirs ~env = List.map (make_dir ~env)
 
-let parse str =
+let parse str : (Command.t, Command.error) Result.t =
   let lexbuf = Lexing.from_string str in
   try Ok (Parser.main Cli_lexer.token lexbuf) with
   | Cli_lexer.Syntax_error tok ->
+      (* I don't know of an easy way to integrate diagnostics reporting with Nottui, see
+         https://github.com/RedPRL/asai/issues/150
+      *)
       Reporter.emitf ~loc:(Range.of_lexbuf lexbuf) Parse_error
         "unrecognized token %S" tok;
-      Error ""
+      Error (Unknown_command tok)
   | Parser.Error ->
       Reporter.emitf ~loc:(Range.of_lexbuf lexbuf) Parse_error "failed to parse";
-      Error ""
+      Error (Unknown_command str)
 
 let setup_cache ~env =
   let cache_path = Eio.Path.(env#cwd / "build") in
@@ -29,55 +31,111 @@ let setup_cache ~env =
   let repo = Irmin_defs.Store.Backend.Repo.v cache_config in
   Irmin_defs.Store.main repo
 
-let handle ~input ~update_history ~quit =
+let handle ~input ~quit =
   let cache = Cache_handle.get () in
   let cmd = parse input in
   match cmd with
   | Ok cmd -> (
       match cmd with
-      | Edit addr ->
-          let tree = Cache.get_value addr cache in
-          let o = Code.show tree.code in
-          update_history (input, Widget.string o)
-      | View addr ->
-          let tree = Cache.get_value addr cache in
-          let o = Widget.string @@ Code.show tree.code in
-          update_history (input, o)
-      | Help ->
-          let o =
-            Widget.string @@ "help cmd is unimplemented. CTRL-Q to quit"
+      | Edit addr -> (
+          let tree = Cache.get_value_opt addr cache in
+          match tree with
+          | Some tree ->
+              (* let o = Components.render_source tree in *)
+              (* Code.show tree.code |> Widget.string |> Lwd.return in *)
+              let o = Code.show tree.code in
+              print_endline o;
+              (input, o)
+          | None ->
+              let err =
+                Command.Tree_not_found addr |> Command.show_error
+                (* Command.Tree_not_found addr |> Components.render_error *)
+                (* |> Lwd.return *)
+              in
+              (input, err))
+      | View addr -> (
+          let tree = Cache.get_artifact_opt addr cache in
+          match tree with
+          | Some tree ->
+              let o =
+                (* Render_tui.render_article tree *)
+                (* Widget.string *)
+                Format.asprintf "%a" Xml_tree2.(pp_article pp_content) tree
+              in
+              (input, o)
+          | None ->
+              let err =
+                Command.Tree_not_found addr |> Command.show_error
+                (* |> Components.render_error |> Lwd.return *)
+              in
+              (input, err))
+      | Help -> (input, "TODO: implement help")
+      | Ls ->
+          let map = Cache.get_artifacts cache in
+
+          let trees =
+            Addr_map.fold (fun addr content acc -> content :: acc) map []
           in
-          update_history (input, o)
-      | Nop ->
-          let o = Widget.string "" in
-          update_history (input, o)
-      | Quit -> quit $= true)
-  | Error _ -> ()
+          (* let rows = Components.forest_summary trees in *)
+          (input, "TODO: implement ls")
+      | Nop -> (input, "")
+      | Quit ->
+          quit $= true;
+          (input, ""))
+  | Error err -> (input, Command.show_error err)
 
-let root_component ~quit =
-  let open Components in
-  let history = Lwd.var ([] : command_history) in
+let rec user_input prompt cb =
+  match LNoise.linenoise prompt with
+  | None -> ()
+  | Some v ->
+      cb v;
+      user_input prompt cb
 
-  let update_history (item : history_item) =
-    let h = Lwd.peek history in
-    history $= item :: h
-  in
-  let focus_handle = Nottui.Focus.make () in
-  let focus_handler state = function `Tab -> `Handled | _ -> `Unhandled in
-  let pressed_tab = Lwd.var false in
-  Lwd_utils.pack Ui.pack_y
-    [
-      Lwd.bind ~f:render_cmd_history (Lwd.get history);
-      input ~last_command:None ~focus_handle ~f:(fun string ->
-          handle ~input:string ~update_history ~quit);
-    ]
+let banner = {| |}
 
 let run ~env ~config_file =
-  let config = Forester_frontend.Config.parse_forest_config_file config_file in
+  let config = Config.parse_forest_config_file config_file in
   let tree_dirs = make_dirs ~env Config.Forest_config.(config.trees) in
   let cache = setup_cache ~env in
   let quit = Lwd.var false in
   Cache_handle.run ~init:cache @@ fun () ->
   let _ = Cache.update_values cache tree_dirs in
-  Ui_loop.run ~quit_on_ctrl_q:true ~quit_on_escape:false ~quit
-    (root_component ~quit)
+  let forest = Forest_reader.read_trees ~env @@ Cache.codes cache tree_dirs in
+  let _ = Cache.persist_addr_map forest cache in
+
+  LNoise.set_hints_callback (fun line ->
+      if line <> "git remote add " then None
+      else
+        Some
+          ( " <this is the remote name> <this is the remote URL>",
+            LNoise.Yellow,
+            true ));
+  (* LNoise.history_load ~filename:"history.txt" |> ignore; *)
+  (* LNoise.history_set ~max_length:100 |> ignore; *)
+  LNoise.set_completion_callback (fun line_so_far ln_completions ->
+      if line_so_far <> "" && line_so_far.[0] = 'h' then
+        [ "Hey"; "Howard"; "Hughes"; "Hocus" ]
+        |> List.iter (LNoise.add_completion ln_completions));
+  [
+    "   __                    _            ";
+    "  / _| ___  _ __ ___ ___| |_ ___ _ __ ";
+    " | |_ / _ \\| '__/ _ / __| __/ _ | '__|";
+    " |  _| (_) | | |  __\\__ | ||  __| |   ";
+    " |_|  \\___/|_|  \\___|___/\\__\\___|_|   ";
+    "";
+  ]
+  |> List.iter print_endline;
+  (fun input ->
+    let update_history _ = () in
+    let res = handle ~input ~quit in
+    Printf.sprintf "%s" (snd res) |> print_endline
+    (* if from_user = "quit" then exit 0; *)
+    (* LNoise.history_add from_user |> ignore; *)
+    (* LNoise.history_save ~filename:"history.txt" |> ignore; *)
+    (* Printf.sprintf "Got: %s" from_user |> print_endline *))
+  |> user_input "forest❯ "
+
+(*
+  Nottui.Ui_loop.run ~quit_on_ctrl_q:true ~quit_on_escape:false ~quit
+    (Components.root ~quit ~handle)
+    *)
