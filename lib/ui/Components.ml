3ea0e00da2b7eec9a902554b6a8ececf91d8a200
Kento Okura <kentookura@gmail.com>
2024-08-08 09:35:14 +0000

    add components for posterity

--- lib/ui/Components.ml
+++ lib/ui/Components.ml
@@ -1,10 +1,12 @@
 open Forester_core
+open Forester_render
+open Forester_frontend
 open Lwd_infix
 open Notty
 open Nottui
 module Widget = Nottui_widgets
 
-type history_item = string * ui
+type history_item = string * ui Lwd.t
 type command_history = history_item list
 
 let prompt ~prompt_label =
@@ -13,6 +15,49 @@ let prompt ~prompt_label =
     I.string A.(fg green) "❯ " |> Ui.atom |> Lwd.return;
   ]
   |> Widget.hbox
+
+let empty = Widget.empty_lwd
+
+let help =
+  let commands =
+    let open Command in
+    [ Quit; Edit (User_addr "<addr>"); View (User_addr "<addr>"); Help; Ls ]
+  in
+  let render_command_help cmd =
+    match (cmd : Command.t) with
+    | Command.Quit -> I.strf "quit %s" "" |> Ui.atom
+    | Command.Edit addr -> I.strf "edit %a" pp_addr addr |> Ui.atom
+    | Command.View addr -> I.strf "view %a" pp_addr addr |> Ui.atom
+    | Command.Help -> I.strf "help %s" "" |> Ui.atom
+    | Command.Ls -> I.strf "ls %s" "" |> Ui.atom
+    | Command.Nop -> I.strf "nop %s" "" |> Ui.atom
+  in
+  Ui.vcat
+    (Ui.atom (I.strf "Available commands")
+    :: (commands |> List.map render_command_help))
+
+let render_error (e : Command.error) : ui =
+  match e with
+  | Command.Unknown_command str ->
+      Ui.hcat [ I.strf "unknown command %s" str |> Ui.atom ]
+  | Command.Tree_not_found addr ->
+      Ui.hcat
+        [
+          I.strf "tree at address %a could not be found" pp_addr addr |> Ui.atom;
+        ]
+  | Command.Custom string -> Ui.hcat [ I.string A.(empty) string |> Ui.atom ]
+
+let render_source (t : Code.tree) =
+  Formatter.format_code t.code
+  |> Formatter.P.pretty_format |> Widget.string |> Lwd.return
+
+let forest_summary trees =
+  trees
+  |> List.map (fun tree ->
+         match tree with
+         | Xml_tree2.{ frontmatter; mainmatter; backmatter } ->
+             Render_tui.render_frontmatter frontmatter)
+  |> Widget.vbox
 
 let input ~last_command ~focus_handle ~f : ui Lwd.t =
   let edit_field = Lwd.var ("", 0) in
@@ -37,7 +82,7 @@ let input ~last_command ~focus_handle ~f : ui Lwd.t =
          ];
     ]
 
-let render_cmd_history : command_history -> ui Lwd.t =
+let cmd_history : command_history -> ui Lwd.t =
  fun items ->
   items
   |> List.map (fun (input, output) ->
@@ -48,7 +93,7 @@ let render_cmd_history : command_history -> ui Lwd.t =
                  prompt ~prompt_label:"Forest";
                  Lwd.return @@ Nottui_widgets.string input;
                ];
-             output |> Lwd.return;
+             output;
            ])
   |> List.rev |> Widget.vbox
 
@@ -65,3 +110,22 @@ let autocomplete (suggestions : (addr * string) list) f =
     Ui.mouse_area (handler addr) i |> Lwd.return
   in
   Widget.hbox @@ List.map item suggestions
+
+let root ~quit ~handle =
+  let history = Lwd.var ([] : command_history) in
+
+  let update_history (item : history_item) =
+    let h = Lwd.peek history in
+    history $= item :: h
+  in
+  let focus_handle = Nottui.Focus.make () in
+  let focus_handler state = function `Tab -> `Handled | _ -> `Unhandled in
+  (* TODO: Figure out this Nottui gravity thing so that we can see the prompt
+     even when the rendered output is long*)
+  Nottui_widgets.scroll_area
+  @@ Lwd_utils.pack Ui.pack_y
+       [
+         Lwd.bind ~f:cmd_history (Lwd.get history);
+         input ~last_command:None ~focus_handle ~f:(fun string ->
+             handle ~input:string ~update_history ~quit);
+       ]
---
+++ lib/ui/Render_tui.ml
@@ -0,0 +1,111 @@
+open Forester_core
+open Forester_prelude
+open Xml_tree2
+
+(*Ideally we avoid Nottui*)
+open Lwd
+open Nottui
+open Notty
+module Widget = Nottui_widgets
+
+let render_xml_elt (elt : 'content xml_elt) =
+  match elt with { name; attrs; content } -> Lwd.return Ui.empty
+
+let render_transclusion (trans : transclusion) =
+  match trans with { addr; target } -> Lwd.return Ui.empty
+
+let render_section (section : 'content section) =
+  match section with { frontmatter; mainmatter } -> Lwd.return Ui.empty
+
+let render_tex ((mode, content) : math_mode * content) =
+  match mode with Inline | Display -> Lwd.return Ui.empty
+
+let render_section (section : 'content section) =
+  match section with { frontmatter; mainmatter } -> Lwd.return Ui.empty
+
+let render_cs (cs : TeX_cs.t) =
+  match cs with TeX_cs.Word _ | TeX_cs.Symbol _ -> Lwd.return Ui.empty
+
+let render_img (img : img) =
+  match img with Inline _ | Remote _ -> Lwd.return Ui.empty
+
+let render_link (link : 'content link) =
+  match link with { href; content } -> Lwd.return Ui.empty
+
+let rec render_node ?attr (n : content_node) =
+  match n with
+  | Text str -> Widget.string ?attr str |> Lwd.return
+  | CDATA str -> Widget.string str |> Lwd.return
+  | Xml_elt elt -> render_xml_elt elt
+  | Transclude transclusion -> render_transclusion transclusion
+  | Results_of_query _ -> Lwd.return Ui.empty
+  | Section section -> render_section section
+  | Prim (prim, content) -> render_prim (prim, content)
+  | KaTeX (mode, content) -> render_tex (mode, content)
+  | TeX_cs cs -> render_cs cs
+  | Link link -> render_link link
+  | Img img -> render_img img
+
+and render_prim ((prim, content) : Prim.t * content) =
+  let attr =
+    match prim with
+    | `Figure -> A.empty
+    | `Em -> A.italic |> A.st
+    | `Figcaption -> A.empty
+    | `Strong -> A.bold |> A.st
+    | `Ul -> A.empty
+    | `Li -> A.empty
+    | `Blockquote -> A.empty
+    | `Code -> A.empty
+    | `Ol -> A.empty
+    | `Pre -> A.empty
+    | `P -> A.empty
+  in
+  Widget.hbox (List.map (fun n -> render_node ~attr n) content)
+
+let render_addr addr =
+  let attr =
+    let open A in
+    bg green ++ fg black
+  in
+  let content =
+    match addr with User_addr str -> str | Machine_addr i -> Int.to_string i
+  in
+  I.strf ~attr " %s " content
+
+let render_frontmatter = function
+  | {
+      addr;
+      title;
+      dates;
+      attributions;
+      taxon;
+      number;
+      physical_parent;
+      designated_parent;
+      source_path;
+      tags;
+      metas;
+    } ->
+      let taxon =
+        match taxon with
+        | Some taxon ->
+            I.strf " %s " (String_util.sentence_case taxon)
+            |> Ui.atom |> Lwd.return
+        | None -> Ui.empty |> Lwd.return
+      in
+      Widget.hbox [ render_addr addr |> Ui.atom |> Lwd.return; taxon ]
+
+let render_backmatter sections =
+  Widget.hbox @@ List.map (fun section -> render_node section) sections
+
+let render_mainmatter mainmatter = Widget.hbox (List.map render_node mainmatter)
+
+let render_article = function
+  | { frontmatter; mainmatter; backmatter } ->
+      Widget.vbox
+        [
+          render_frontmatter frontmatter;
+          render_mainmatter mainmatter;
+          render_backmatter backmatter;
+        ]
